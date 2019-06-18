24282c2d3bf90023df9ae8c92ec8174600b70f3e
Kai Wang <charles.wangkai@gmail.com>
2019-06-18 19:14:20 +0800

    Leapfrog: Ch. 1

---
+++ 2019/qualification/Leapfrog: Ch. 1/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			String L = sc.next();
+
+			System.out.println(String.format("Case #%d: %c", tc, solve(L) ? 'Y' : 'N'));
+		}
+
+		sc.close();
+	}
+
+	static boolean solve(String L) {
+		int dotCount = (int) L.chars().filter(ch -> ch == '.').count();
+		int bCount = (int) L.chars().filter(ch -> ch == 'B').count();
+
+		return dotCount != 0 && bCount >= dotCount;
+	}
+}
